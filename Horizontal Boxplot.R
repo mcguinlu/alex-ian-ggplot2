2cb37bffab63cfa1195d4411beeadfe344fa534e
Luke McGuinness <41202585+mcguinlu@users.noreply.github.com>
2019-03-11 17:51:07 +0000

    Updated Version

--- Horizontal Boxplot.R
+++ Horizontal Boxplot.R
@@ -1,26 +1,99 @@
-df <- data.frame(dose=c("D0.5", "D1", "D2", "D3","D4", "D5"),
-                 len=c(4.2, 10, 29.5, 20, 12, 112)
-                 )
-p<-ggplot(data=df, aes(x=dose, y=len)) +
-  geom_bar(stat="identity") +
-  coord_flip()
-p
+MyData = read.csv("O:/Documents/Stuff/Projects/rrr/Case reports_V2.csv", header = TRUE)
+MyData$Duration <- sub("^$", 0, MyData$Duration)
+MyData$Duration <- sub("<1", 0.5, MyData$Duration)
+MyData$Duration <- as.numeric(MyData$Duration)
+
+MyData$PFS <- sub("^$", NA, MyData$PFS)
+MyData$PFS <- sub("<1", 0.5, MyData$PFS)
+MyData$PFS <- sub("Not reported", NA, MyData$PFS)
+MyData$PFS <- as.numeric(MyData$PFS)
+
+MyData$OS <- sub("^$", NA, MyData$OS)
+MyData$OS <- sub("<1", 0.5, MyData$OS)
+MyData$OS <- sub("Not reported", NA, MyData$OS)
+MyData$OS <- as.numeric(MyData$OS)
+
+#Create variable to position text
+MyData$labelpos <- rep(-10,times=nrow(MyData))
+
+#Create new variable to represent ongoing treatment
+MyData$Ongoing<- sub("^$", NA, MyData$Ongoing)
+MyData$arrowhead <- ifelse((is.na(MyData$Ongoing) & is.na(MyData$PFS) & is.na(MyData$OS)), MyData$Duration+0.2, NA)
+MyData$arrowbody <- ifelse((is.na(MyData$Ongoing) & is.na(MyData$PFS) & is.na(MyData$OS)), MyData$Duration+0.1, NA)
+
+NewData <- MyData[which(MyData$Duration!=0),]
+NewData <- NewData[order(NewData$BRAF.inhibitor),]
+NewData$ï..Patient.ID <- seq(1,nrow(NewData),by=1)
 
 
-MyData = data.frame(
-  method=rep(c("A","2","3","4","5","6","7","8","9","10"), times = 3),
-  time1=rnorm(30,10,3),
-  time2=rnorm(30,8,2),
-  group=rep(c("PXA","Other Drug","Drug 3"),each=10),
-  cat=rep(c(1,2),each=15)
-)
 
-p2 <- ggplot(MyData, aes(group, time1,  fill = as.factor(method))) + 
-  geom_bar(position=position_dodge2(width = 0.5), stat="identity", show.legend = FALSE) + 
-  coord_flip() +
-  geom_hline(time1, yintercept = 5, linetype="dashed")+
-  geom_hline(time1, yintercept = 10, linetype="dashed")+
-  geom_hline(time1, yintercept = 15, linetype="dashed") +
-  scale_fill_manual(values = c(rep("blue",times=10)))
+
+p2 <- ggplot(NewData, aes(x=ï..Patient.ID, y=Duration, fill = BRAF.inhibitor)) + 
   
+  #Bar chart
+  geom_bar(position=position_dodge2(width = 0.9, preserve = "single"), 
+           stat="identity", 
+           color = "black", 
+           width = 0.8) + 
+  
+  #Points for overall survival
+  geom_point(aes(y=OS),
+             stat="identity",
+             position=position_dodge2(width = 0.9, preserve = "single"),
+             shape = 10,
+             colour = "black",
+             size=2, 
+             show.legend = FALSE) +
+  
+  #Points for progression free survival
+  geom_point(aes(y=PFS),
+             stat="identity",
+             position=position_dodge2(width = 0.9, preserve = "single"),
+             shape = 16,
+             colour = "black",
+             size=2, 
+             show.legend = FALSE) +
+  
+  #Create "ongoing" arrow
+  geom_point(aes(y=arrowhead),
+             stat="identity",
+             position=position_dodge2(width = 0.9, preserve = "single"),
+             shape = 62,
+             colour = "black",
+             size=3, 
+             show.legend = FALSE) +
+  geom_point(aes(y=arrowbody),
+             stat="identity",
+             position=position_dodge2(width = 0.9, preserve = "single"),
+             shape = 95,
+             colour = "black",
+             size=3, 
+             show.legend = FALSE) +
+  
+  annotate("text", x=NewData$ï..Patient.ID, y=NewData$labelpos, label = NewData$Reference, size = 2) +
+  
+  #Flip orientation of boxplot
+  coord_flip() +
+  
+  #Add vertical lines every 6 months
+  geom_hline(time1, yintercept = 6, linetype="dashed")+
+  geom_hline(time1, yintercept = 12, linetype="dashed")+
+  geom_hline(time1, yintercept = 18, linetype="dashed") +
+  geom_hline(time1, yintercept = 24, linetype="dashed") +
+  
+  #Add chart labels, and remove labels from x-axis
+  labs(title="Survival", 
+       x="", y = "Duration (Months)")+
+    theme(axis.ticks.y = element_blank(),
+        legend.title=element_blank()) +
+
+  #Add colour scheme
+  scale_fill_brewer(palette = "Blues")
+  
+  
+#Plot chart
 p2
+
+ggsave("C:/Users/lm16564/Downloads/horboxplot.jpeg", dpi=600)
+
+
